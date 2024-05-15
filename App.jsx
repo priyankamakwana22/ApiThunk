cd3d00c9f3ed09309723eec0152e7f159ae79bf2
Panther Black <pantherblack@Panthers-Mac-mini.local>
2024-05-15 18:29:00 +0530

    Made api call in reducer file function

--- App.jsx
+++ App.jsx
@@ -1,35 +1,12 @@
-import React, {useEffect} from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
-import {getDataAsync, getDataMethod} from './src/redux/Action';
+import {View} from 'react-native';
+import ThunkScreen from './src/screens/ThunkScreen';
 
 const App = () => {
-  const data = useSelector(state => state.data);
-  console.log('🚀 ~ App ~ dataFromApi:', data);
-  console.log('Hello');
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getDataAsync());
-  }, []);
-
   return (
-    <View>
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <View>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-            <Text>{item.description}</Text>
-          </View>
-        )}
-      />
-    </View>
+    
+      <ThunkScreen />
+      
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-});
---
+++ src/screens/ThunkScreen.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import { getDataAsync } from '../redux_thunk/Action';
+
+const ThunkScreen = () => {
+  const data = useSelector(state => state.data);
+  console.log('🚀 ~ App ~ dataFromApi:', data);
+  console.log('Hello');
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDataAsync());
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.price}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default ThunkScreen;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
